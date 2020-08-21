7af752bec1c4a48bf077d164916fbaca9c045fcd
Avinash Dixit <avinashdixit499@gmail.com>
2020-08-21 21:40:10 +0530

    added timer in homepage

--- changes/models.py
+++ changes/models.py
@@ -9,10 +9,6 @@ class Shophome(models.Model):
 
 class HotDeals(models.Model):
     image = models.ImageField(default='hotdeal.png',upload_to='product_pics')
-    days = models.PositiveIntegerField()
-    hours = models.PositiveIntegerField()
-    minutes = models.PositiveIntegerField()
-    seconds = models.PositiveIntegerField()
     heading = models.CharField(max_length = 70)
     details = models.CharField(max_length = 100)
     datetime = models.DateTimeField(auto_now_add=False,blank=True,null=True)
